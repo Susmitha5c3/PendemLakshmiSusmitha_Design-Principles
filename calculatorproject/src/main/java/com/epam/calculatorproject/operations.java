75f54cfab57a8f37a8a2ba22db47302beac4e2e5
Pendem Lakshmi Susmitha <lakshmisusmitha13@gmail.com>
2020-03-16 21:34:46 +0530

    Add files via upload

---
+++ calculatorproject/src/main/java/com/epam/calculatorproject/operations.java
@@ -0,0 +1,9 @@
+package com.epam.calculatorproject;
+
+public  abstract class Operations {
+	public abstract double add(double a, double b);
+	public abstract double subtract(double a, double b);
+	public abstract double multiply(double a, double b);
+	public abstract double divide(double a, double b);
+
+}
